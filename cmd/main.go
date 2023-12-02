222212f2b8101f2b45704a84ed6ed9270329d284
whkelvin <whkelvin.hsu@gmail.com>
2023-12-02 15:57:38 -0400

    able to run a command on a remote computer

--- cmd/main.go
+++ cmd/main.go
@@ -2,8 +2,57 @@ package main
 
 import (
 	"fmt"
+	"golang.org/x/crypto/ssh"
+	"io/ioutil"
+	"log"
 )
 
 func main() {
-	fmt.Println("hello")
+	host := "192.168.2.121"
+	port := "22"
+	user := "whkelvin"
+	privateKeyPath := "/home/whkelvin/.ssh/id_rsa_orca"
+	password := "your password here"
+
+	privateKey, err := ioutil.ReadFile(privateKeyPath)
+	if err != nil {
+		log.Fatalf("Failed to load private key: %v", err)
+	}
+
+	// Create the SSH configuration
+	signer, err := ssh.ParsePrivateKeyWithPassphrase(privateKey, []byte(password))
+	if err != nil {
+		log.Fatalf("Failed to parse private key: %v", err)
+	}
+
+	sshConfig := &ssh.ClientConfig{
+		User: user,
+		Auth: []ssh.AuthMethod{
+			ssh.PublicKeys(signer),
+		},
+		HostKeyCallback: ssh.InsecureIgnoreHostKey(), // WARNING: Insecure, use for testing purposes only
+	}
+
+	// Connect to the SSH server
+	client, err := ssh.Dial("tcp", host+":"+port, sshConfig)
+	if err != nil {
+		log.Fatalf("Failed to dial: %v", err)
+	}
+	defer client.Close()
+
+	// Example: Run a command on the remote server
+	session, err := client.NewSession()
+	if err != nil {
+		log.Fatalf("Failed to create session: %v", err)
+	}
+	defer session.Close()
+
+	// Replace "your_command" with the command you want to execute
+	output, err := session.CombinedOutput("ls -la")
+	if err != nil {
+		log.Fatalf("Failed to run command: %v", err)
+	}
+
+	fmt.Println(string(output))
+
 }
--- go.mod
+++ go.mod
@@ -1,3 +1,7 @@
 module poc
 
 go 1.19
+
+require golang.org/x/crypto v0.16.0
+
+require golang.org/x/sys v0.15.0 // indirect
